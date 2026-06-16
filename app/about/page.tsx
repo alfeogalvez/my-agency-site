b66f9f80673ec94b122a430928bed04534c57989
HelloVA Admin <ez@gmail.com>
2026-06-16 11:03:44 +0800

    Increased footer social icons size, added official brand colors, and scaled up footer logo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -104,11 +104,13 @@ export default function About() {
       {/* FOOTER */}
       <footer className="bg-black pt-12 pb-8 px-6 border-t border-gray-800">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-start gap-8">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
             <a href="/" className="block w-fit">
-              <img src="/logo-footer.png" alt="HelloVA Logo" className="h-16 w-auto object-contain rounded-xl" />
+              <img src="/logo-footer.png" alt="HelloVA Logo" className="h-48 w-auto object-contain rounded-xl" />
             </a>
-            <p className="text-gray-400 text-sm max-w-xs">Remote support solutions for growing businesses.</p>
+            <p className="text-gray-400 text-sm max-w-xs -mt-2">
+              Remote support solutions for growing businesses.
+            </p>
           </div>
           <div className="grid grid-cols-2 md:grid-cols-2 gap-8">
             <div>
@@ -131,7 +133,7 @@ export default function About() {
                   <svg className="w-[30px] h-[30px]" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4s1.791-4 4-4 4 1.79 4 4-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
                 </a>
                 <a href="https://facebook.com/hellova.solutions" target="_blank" rel="noreferrer" className="text-[#1877F2] hover:opacity-80 transition hover:scale-110">
-                  <svg className="w-[30px] h-[30px]" fill="currentColor" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
+                  <svg className="w-[30px] h-[30px]" fill="currentColor" viewBox="0 0 24 24"><path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"/></svg>
                 </a>
               </div>
             </div>
